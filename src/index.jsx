38cfa3ca3643974712eb6bce414f5e916ddd1ed5
Jinhyeok <jhlee1@c-won.com>
2016-04-09 15:18:08 +0900

    random documentation testing

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,19 @@
+// # EmitTimestampPlugin
+// There were some plugins that intends to fix the same issue
+// but none of them were emitting the result properly, per spec.
 function EmitTimestampPlugin( {
   path = '',
   filename = 'timestamp.json',
   beautify = false,
-} = {}) {
+} = {} ) {
   this.options = {
     path,
     filename,
     beautify,
   };
 }
-
+// # private: `_getTimestampObject`
+// Make an object we desire
 EmitTimestampPlugin.prototype._getTimestampObject = function () {
   const fnZero = ( n, len = 2 ) => '0'.repeat( Math.max( 0, len - String( n ).length ) ) + n;
   const dtNow = new Date();
@@ -53,18 +57,17 @@ EmitTimestampPlugin.prototype._getTimestampObject = function () {
   return oResult;
 };
 
+// # public: `apply`
+// Webpack will ask EmitTimestampPlugin.apply to plug a callback before processing.
 EmitTimestampPlugin.prototype.apply = function ( compiler ) {
   compiler.plugin( 'emit', ( compilation, callback ) => {
     const result = JSON.stringify( this._getTimestampObject(), false, this.options.beautify ? 2 : false );
-
-    // Insert this list into the Webpack build as a new file asset:
+    // Tells webpack to include `path`+`filename` asset, as in spec.
     compilation.assets[ this.options.path + this.options.filename ] = {
       source: () => result,
       size: () => result.length,
     };
-
     callback();
   } );
 };
-
 module.exports = EmitTimestampPlugin;
